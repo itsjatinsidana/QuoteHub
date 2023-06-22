a198fd71947ddda0ded926ab61e87b9423caffe5
jatin sidana <jatinsadana1998@gmail.com>
2023-06-22 15:55:48 +0530

    navbar completed

--- qoutehub/src/compnents/Navbar.js
+++ qoutehub/src/compnents/Navbar.js
@@ -1,24 +1,23 @@
-import {Link} from "react-router-dom";
-import {  Nav } from 'react-bootstrap';
-export const Navbar = () =>{
-    return(
+import { Link } from "react-router-dom";
+export const Navbar = () => {
+    return (
         <>
-        <Nav>
-            <div className="title-container">
-              <Link to={'/'}>
-              <span className="title">QouteHub</span>
-              </Link>
-            </div>
-            <div className="list-items">
-                <ul>
-                <li className="list-items"><Link to={'/qoutes'}> Qoutes</Link></li>
-                <li className="list-items"><Link to={'/collections'}>Collection</Link></li>
-                <li className="list-items"><Link to={'/authors'}>Authors</Link></li>
-                </ul>
+            <nav>
+                <div className="title-container">
+                    <Link to={'/'}>
+                        <span className="title">QouteHub</span>
+                    </Link>
+                </div>
+                <div className="list-items">
+                    <ul>
+                        <li className="list-item"><Link to={'/qoutes'}> Qoutes</Link></li>
+                        <li className="list-item"><Link to={'/collections'}>Collection</Link></li>
+                        <li className="list-item"><Link to={'/authors'}>Authors</Link></li>
+                    </ul>
 
-            </div>
+                </div>
 
-        </Nav>
+            </nav>
         </>
     )
 }
