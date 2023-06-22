72a52a173d70da2915f7f958a91c5d7a4a6d2263
jatin sidana <jatinsadana1998@gmail.com>
2023-06-22 17:31:33 +0530

    designed home

--- qoutehub/src/pages/Home.js
+++ qoutehub/src/pages/Home.js
@@ -1,11 +1,20 @@
 import { Navbar } from "../compnents/Navbar"
 
 export const Home = () => {
-    return(
+    return (
         <>
-        <div className="navbar-container">
-           <Navbar/>
-        </div>
+            <div className="navbar-container">
+                <Navbar />
+            </div>
+            <div class="home-card">
+                <span className="span1">Unlock the power of words at QuoteHub </span>
+                
+                <input className="search-field" type="text" placeholder="search here"/>
+                <hr/>
+                <button className="btn-clr">Explore Qoutes</button>
+                
+            </div>
+
         </>
     )
 }
