1c13941b4896155c211f6e0a2a3f2bc03ed9839e
jatin sidana <jatinsadana1998@gmail.com>
2023-06-25 14:10:32 +0530

    pagination completed

---
+++ qoutehub/src/compnents/Loader.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+const Loader = () => {
+    return (
+        <div className='lds-roller'>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    );
+};
+
+export default Loader;
--- qoutehub/src/pages/Qoutes.js
+++ qoutehub/src/pages/Qoutes.js
@@ -1,9 +1,13 @@
 import { useEffect, useState } from "react";
 import { Navbar } from "../compnents/Navbar";
 import axios from 'axios';
+import Loader from "../compnents/Loader";
+
 
 export const Qoutes = () => {
     const [qoute, setQoute] = useState([]);
+    const [page, setPage] = useState(1);
+    const [loding, setLoading] = useState(true);
     async function getQoutes() {
 
 
@@ -16,22 +20,34 @@ export const Qoutes = () => {
                 'X-RapidAPI-Host': 'quotel-quotes.p.rapidapi.com'
             },
             data: {
-                pageSize: 150,
-                page: 0
+                pageSize: 10,
+                page: page
             }
         };
 
         try {
             const response = await axios.request(options);
             console.log(response.data);
-            setQoute(response.data)
+            setQoute(prev => [...prev, ...response.data])
+            setLoading(false)
         } catch (error) {
             console.error(error);
         }
     }
     useEffect(() => {
         getQoutes()
-    }, [])
+    }, [page])
+    function handleScroll() {
+
+        if (window.innerHeight + document.documentElement.scrollTop + 1 >= document.documentElement.scrollHeight) {
+            setLoading(true);
+            setPage(prev => prev + 1)
+        }
+    }
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
     return (
         <>
             <div className="qoutes-nav">
@@ -43,9 +59,12 @@ export const Qoutes = () => {
                         return (
                             <>
                                 <div className="card-content" key={index}>
+
                                     <blockquote className="quote">{value.quote}</blockquote>
                                     <p className="author"> {value.name}</p>
+
                                 </div>
+
                             </>
                         )
                     }))
@@ -53,9 +72,10 @@ export const Qoutes = () => {
 
 
 
-
+               
 
             </div >
+            <Loader />
         </>
     )
 }
